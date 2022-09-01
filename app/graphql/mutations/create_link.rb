d458bd38fbc4a3dea38fb38765555cb9b1d6e54e
solar05 <artem00298@gmail.com>
2022-09-01 18:15:47 +0300

    Add errors handling

--- app/graphql/mutations/create_link.rb
+++ app/graphql/mutations/create_link.rb
@@ -9,6 +9,9 @@ module Mutations
 
     def resolve(description: nil, url: nil)
       Link.create!(description: description, url: url, user: context[:current_user])
+
+    rescue ActiveRecord::RecordInvalid => e
+        GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
   end
 end
