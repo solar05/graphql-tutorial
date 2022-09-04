f01bbbd384e7500d3b37a7c62d1ef9a74266e966
solar05 <artem00298@gmail.com>
2022-09-04 16:29:33 +0300

    Add show link

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_links, resolver: Resolvers::LinksSearch
+
+    field :link, Types::LinkType, null: false do
+      argument :id, ID, required: true
+    end
+    
+    def link(id:)
+      Link.find(id)
+    end
   end
 end
